67d38dc727d566a95c156da11042b57fcdf3033f
Israel Herrera Payano <kv_el@outlook.com>
2023-10-26 18:45:53 -0400

    Date Form

--- src/actors/ActorsEdit.tsx
+++ src/actors/ActorsEdit.tsx
@@ -4,7 +4,7 @@ export default function ActorsEdit() {
     return (
         <>
             <h3>Edit Actors</h3>
-            <ActorsForm model={{ name: 'Tom Holland', birthDate: new Date('1996-06-01-T:00:00:00') }} onSubmit={values => console.log(values)} />
+            <ActorsForm model={{ name: 'Tom Holland', birthDate: new Date('1996-06-01T00:00:00') }} onSubmit={values => console.log(values)} />
         </>
     )
 }
--- src/actors/ActorsForm.tsx
+++ src/actors/ActorsForm.tsx
@@ -4,19 +4,22 @@ import FormGroupText from "../utils/FormGroupText";
 import Button from "../utils/Button";
 import { Link } from "react-router-dom";
 import * as Yup from 'yup';
+import FormGroupDate from "../utils/FormGroupDate";
 
 export default function ActorsForm(props: actorsFormProps) {
     return (
         <>
             <Formik initialValues={props.model} onSubmit={props.onSubmit}
                 validationSchema={Yup.object({
-                    name: Yup.string().required('This field is required').firstCapLetter()
+                    name: Yup.string().required('This field is required').firstCapLetter(),
+                    birthDate: Yup.date().nullable().required('This field is required')
                 })}
             >
 
                 {(formikProps) => (
                     <Form>
                         <FormGroupText field="name" label="Name" />
+                        <FormGroupDate field="birthDate" label="Date of birth" />
                         <Button disabled={formikProps.isSubmitting} type="submit">Save</Button>
                         <Link className="btn btn-secondary" to="/actors">Cancel</Link>
                     </Form>
