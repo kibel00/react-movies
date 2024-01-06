687935d9fb0e7d6abb5c3203b73f77518a44bd65
Israel Herrera Payano <kv_el@outlook.com>
2024-01-06 11:59:01 -0400

    Install axios, configure and configure enviroment variable

--- src/genres/GenresIndex.tsx
+++ src/genres/GenresIndex.tsx
@@ -1,7 +1,17 @@
+import axios, { AxiosResponse } from "axios";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { genresDTO } from "./Genres.Model";
+import { genderUrl } from "../utils/endpoints";
 
-export default function GenresIndex(){
-    return(
+export default function GenresIndex() {
+    useEffect(() => {
+        axios.get(genderUrl)
+            .then((answer: AxiosResponse<genresDTO[]>) => {
+                console.log(answer.data)
+            })
+    }, [])
+    return (
         <>
             <h3>Genres Index</h3>
             <Link to="/genres/create">Create Genres</Link>
