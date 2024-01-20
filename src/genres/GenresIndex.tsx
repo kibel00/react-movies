7343011d3b0db43c96dc81c70841d62df044a2eb
Israel Herrera Payano <kv_el@outlook.com>
2024-01-20 14:03:09 -0400

    Gender Index

--- src/genres/GenresIndex.tsx
+++ src/genres/GenresIndex.tsx
@@ -1,20 +1,49 @@
 import axios, { AxiosResponse } from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { genresDTO } from "./Genres.Model";
 import { genderUrl } from "../utils/endpoints";
+import GenericList from "../utils/GenericList";
+import Button from "../utils/Button";
 
 export default function GenresIndex() {
+
+    const [genders, setGenders] = useState<genresDTO[]>();
+
     useEffect(() => {
         axios.get(genderUrl)
             .then((answer: AxiosResponse<genresDTO[]>) => {
-                console.log(answer.data);
+                setGenders(answer.data);
             })
     }, [])
     return (
         <>
             <h3>Genres</h3>
             <Link className="btn btn-primary" to="/genres/create">Create Genres</Link>
+
+            <GenericList list={genders}>
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Action</th>
+                            <th>Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {genders?.map(x => <tr key={x.id}>
+                            <td>
+                                <Link className="btn btn-success" to={`/genres/${x.id}`}>
+                                    Edit
+                                </Link>
+                                <Button className="btn btn-danger">Delete</Button>
+                            </td>
+                            <td>
+                                {x.name}
+                            </td>
+                        </tr>)}
+                    </tbody>
+                </table>
+            </GenericList>
         </>
     )
 }
